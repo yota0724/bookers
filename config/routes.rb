7e223e0e2a33674d1f19c50b0951827221991f3c
EC2 Default User <ec2-user@ip-172-31-24-84.ap-northeast-1.compute.internal>
2024-11-03 18:08:50 +0000

    RSpec test now

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  get '/books' => 'books#index'
   resources :books
   #get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
